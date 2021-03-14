44cfde70cfe9f8f30e7346720c0f5173e9c99887
noelma <noel-mathieu@hotmail.fr>
2021-03-14 19:31:33 +0100

    Refactor, simplification des drivers.

--- src/Driver/Json.php
+++ src/Driver/Json.php
@@ -8,65 +8,23 @@
 
 namespace Queryflatfile\Driver;
 
+use Queryflatfile\Exception\Driver\ExtensionNotLoadedException;
+
 /**
  * Manipule des données dans des fichiers de type JSON
  *
  * @author Mathieu NOËL <mathieu@soosyze.com>
  */
-class Json extends \Queryflatfile\Driver
+final class Json extends \Queryflatfile\Driver
 {
     /**
      * {@inheritDoc}
      */
-    public function create($path, $fileName, array $data = [])
+    public function checkExtension()
     {
-        $this->checkExtension();
-        $file = $this->getFile($path, $fileName);
-
-        if (!file_exists($path)) {
-            mkdir($path, 0755, true);
+        if (!extension_loaded('json')) {
+            throw new ExtensionNotLoadedException('The json extension is not loaded.');
         }
-        if (!file_exists($file)) {
-            $handle = fopen($file, 'w+');
-            fwrite($handle, json_encode($data, JSON_UNESCAPED_UNICODE));
-
-            return fclose($handle);
-        }
-
-        return false;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function read($path, $fileName)
-    {
-        $this->checkExtension();
-        $file = $this->getFile($path, $fileName);
-
-        $this->isExist($file);
-        $this->isRead($file);
-
-        $data = file_get_contents($file);
-
-        return json_decode($data, true, 512, JSON_UNESCAPED_UNICODE);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function save($path, $fileName, array $data)
-    {
-        $this->checkExtension();
-        $file = $this->getFile($path, $fileName);
-
-        $this->isExist($file);
-        $this->isWrite($file);
-
-        $handle = fopen($file, 'w');
-        fwrite($handle, json_encode($data, JSON_UNESCAPED_UNICODE));
-
-        return fclose($handle);
     }
 
     /**
@@ -78,16 +36,18 @@ class Json extends \Queryflatfile\Driver
     }
 
     /**
-     * Déclanche une exception si le l'extension du fichier n'est pas chargée.
-     *
-     * @codeCoverageIgnore has
-     *
-     * @throws Exception\Driver\ExtensionNotLoadedException
+     * {@inheritDoc}
      */
-    private function checkExtension()
+    public function serializeData(array $data)
     {
-        if (!extension_loaded('json')) {
-            throw new Exception\Driver\ExtensionNotLoadedException('The json extension is not loaded.');
-        }
+        return json_encode($data, JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function unserializeData($data)
+    {
+        return json_decode($data, true, 512, JSON_UNESCAPED_UNICODE);
     }
 }
