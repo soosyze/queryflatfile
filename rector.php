04ab72514bdfcc0ad7c6ab46cba59f3955fcdff7
noelma <noel-mathieu@hotmail.fr>
2022-07-05 22:41:16 +0200

    chore: configuration update for rector

--- rector.php
+++ rector.php
@@ -2,27 +2,23 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-
+return static function (RectorConfig $rectorConfig): void {
     // paths to refactor; solid alternative to CLI arguments
-    $parameters->set(Option::PATHS, [
+    $rectorConfig->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests'
     ]);
 
     // is your PHP version different from the one your refactor to? [default: your PHP version], uses PHP_VERSION_ID format
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_72);
+    $rectorConfig->phpVersion(PhpVersion::PHP_72);
 
     // Define what rule sets will be applied
-    $containerConfigurator->import(SetList::CODE_QUALITY);
+    $rectorConfig->sets([SetList::CODE_QUALITY]);
 
     // Path to phpstan with extensions, that PHPSTan in Rector uses to determine types
-    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, getcwd() . '/phpstan.neon.dist');
+    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon.dist');
 };
